71627b522cbd62c8790783f953cd0080ea0715ec
Chandler Trousdale <chandler2506@gmail.com>
2026-09-17 21:56:45 -0400

    oxocarbon -> kanagawa-dragon

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,19 @@
 return {
-	"nyoom-engineering/oxocarbon.nvim",
+	"rebelot/kanagawa.nvim",
+	lazy = false,
+	priority = 1000,
 	build = false,
 	config = function()
 		vim.opt.background = "dark"
-		vim.cmd.colorscheme("oxocarbon")
+		require("kanagawa").setup({
+			transparent = true,
+			theme = "dragon",
+			background = {
+				dark = "dragon",
+				light = "lotus",
+			},
+		})
+		vim.cmd.colorscheme("kanagawa-dragon")
 
 		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
